a565369f0a68a87ef45c3224a13357650edc8fd2
Mahmoud Magdy <ma.ahmed@eakeb.tech>
2024-08-08 19:20:04 +0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "home", element: <p>home</p> },
+      { path: "about", element: <p>about</p> },
+      { path: "*", element: <p>404</p> },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={routes}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </RouterProvider>
+);
